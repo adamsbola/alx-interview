3f19d69b503bde7d2a2108dc01f48838af68ef05
Adamsbola <Adamadu52@gmail.com>
2023-07-19 17:42:23 +0100

    0x03-log_parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,19 +2,19 @@
 
 """Script that reads stdin line by line and computes metrics"""
 
-   import sys
+import sys
 
 
-   def printsts(dic, size):
-       """ WWPrints information """
-       print("File size: {:d}".format(size))
-       for i in sorted(dic.keys()):
-           if dic[i] !=0:
-               print("{}: {:d}".format(i, dic[i]))
+def printsts(dic, size):
+    """ WWPrints information """
+    print("File size: {:d}".format(size))
+    for i in sorted(dic.keys()):
+        if dic[i] != 0:
+            print("{}: {:d}".format(i, dic[i]))
 
 
-sts = {"200": 0, "301": 0, "400": 0, "403": 0,
-        "404": 0, "405": 0, "500": 0}
+sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+       "404": 0, "405": 0, "500": 0}
 
 count = 0
 size = 0
@@ -37,9 +37,9 @@ try:
                 sts[stlist[-2]] += 1
         except:
             pass
-        printsts(sts, size)
+    printsts(sts, size)
 
 
-except keyboardIterrupt:
-    printsts
+except KeyboardInterrupt:
+    printsts(sts, size)
     raise
